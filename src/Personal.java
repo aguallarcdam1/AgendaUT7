f54408156f6c8203c04aff3d7d552a5a8ae00720
aguallarcdam1 <aguallarcdam1@gmail.com>
2021-04-05 20:25:30 +0200

    Creación del enumerado 'Relacion' y realización del constructor, atributos, getters y setters de la clase Personal.

--- src/Personal.java
+++ src/Personal.java
@@ -1,8 +1,26 @@
+import java.time.LocalDate;
 
-public class Personal {
+public class Personal extends Contacto{
+    private LocalDate fechaNacimiento;
+    private Relacion relacion;
+    public Personal(String nombre, String apellidos, String telefono, String email, LocalDate fechaNacimiento, Relacion relacion) {
+	super(nombre, apellidos, telefono, email);
+	this.setFechaNacimiento(fechaNacimiento);
+	this.setRelacion(relacion);
+    }
+    public Relacion getRelacion() {
+	return relacion;
+    }
+    public void setRelacion(Relacion relacion) {
+	this.relacion = relacion;
+    }
+    public LocalDate getFechaNacimiento() {
+	return fechaNacimiento;
+    }
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+	this.fechaNacimiento = fechaNacimiento;
+    }
 
-	public Personal() {
-
-	}
+	
 
 }
---
+++ src/Relacion.java
@@ -0,0 +1,4 @@
+
+public enum Relacion {
+    PADRE, MADRE, AMIGOS, PAREJA, HIJO, HIJA;
+}
