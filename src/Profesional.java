cbc0f8c401a440ff59abb738f5ca0828c0d5d3e0
jmayorodam1 <jmayorodam1@gmail.com>
2021-04-11 12:53:16 +0200

    Hecho el constructor , toString, getters y setters necesarios de la clase Profesional

--- src/Profesional.java
+++ src/Profesional.java
@@ -1,8 +1,32 @@
 
-public class Profesional {
+public class Profesional extends Contacto {
+	private String empresa;
 
-	public Profesional() {
-
+	public Profesional(String nombre, String apellidos, String telefono, String email, String empresa)  {
+			super(nombre, apellidos, telefono, email);
 	}
+
+	@Override
+	public String Firma() {
+		
+		return null;
+	}
+
+	
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+
+	@Override
+	public String toString() {
+		
+		return getApellidos() + " , " + getNombre() + "(PROFESIONAL)\n" + "Tfno: " 
+		+ getTelefono() + " | email: " + getEmail() + "\n Empresa: " + getEmpresa();
+	}
+
 
 }
