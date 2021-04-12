490019009556a9153fa74ff5af7c5f92f2b099b5
aguallarcdam1 <aguallarcgam1@gmail.com>
2021-04-12 15:56:46 +0200

    Correción del método "esCumpleaños"

--- src/Personal.java
+++ src/Personal.java
@@ -4,46 +4,57 @@ import java.time.format.DateTimeFormatter;
 /*
  * autor: Parte de Andrés
  */
-public class Personal extends Contacto{
-    private LocalDate fechaNacimiento;
-    private Relacion relacion;
-    public Personal(String nombre, String apellidos, String telefono, String email, String fechaNacimiento, Relacion relacion) {
-	super(nombre, apellidos, telefono, email);
-	this.setFechaNacimiento(fechaNacimiento);
-	this.setRelacion(relacion);
-    }
-    public Relacion getRelacion() {
-	return relacion;
-    }
-    public void setRelacion(Relacion relacion) {
-	this.relacion = relacion;
-    }
-    public LocalDate getFechaNacimiento() {
-	return fechaNacimiento;
-    }
-    public void setFechaNacimiento(String fechaNacimiento) {
-	this.fechaNacimiento = LocalDate.parse(fechaNacimiento);
-    }
-    @Override
-    public String getFirmaEmail() {
+public class Personal extends Contacto {
+	private LocalDate fechaNacimiento;
+	private Relacion relacion;
 
-	return "Un abrazo!!";
-    }
+	public Personal(String nombre, String apellidos, String telefono, String email, String fechaNacimiento,
+			Relacion relacion) {
+		super(nombre, apellidos, telefono, email);
+		this.setFechaNacimiento(fechaNacimiento);
+		this.setRelacion(relacion);
+	}
 
-    public boolean esCumpleaños() {
-	
-	return fechaNacimiento.equals(LocalDate.now());
-    
-    }
-    
+	public Relacion getRelacion() {
+		return relacion;
+	}
+
+	public void setRelacion(Relacion relacion) {
+		this.relacion = relacion;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = LocalDate.parse(fechaNacimiento);
+	}
+
+	@Override
+	public String getFirmaEmail() {
+
+		return "Un abrazo!!";
+	}
+
+	public boolean esCumpleaños() {
+		LocalDate today = LocalDate.now();
+		if (fechaNacimiento.getDayOfMonth() == today.getDayOfMonth()
+				&& fechaNacimiento.getMonthValue() == today.getMonthValue()) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
 	@Override
 	public String toString() {
-		
-		return super.getApellidos() + ", " + super.getNombre() + " (" + this.getClass().getSimpleName()
-				+ ")\n" + "Tfno: " + super.getTelefono() + " | " + "email: " + super.getEmail() 
-				+ "\nFecha nacimiento: " + this.getFechaNacimiento().format(DateTimeFormatter.ofPattern("dd MMM. yyyy"))
-				+ "\nRelación: " + this.getRelacion();
+
+		return super.getApellidos() + ", " + super.getNombre() + " (" + this.getClass().getSimpleName() + ")\n"
+				+ "Tfno: " + super.getTelefono() + " | " + "email: " + super.getEmail() + "\nFecha nacimiento: "
+				+ this.getFechaNacimiento().format(DateTimeFormatter.ofPattern("dd MMM. yyyy")) + "\nRelación: "
+				+ this.getRelacion();
 	}
 
 }
-
