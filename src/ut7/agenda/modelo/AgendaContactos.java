17cfa3a36027bda7ebd7d87a4d8afd7d858ef642
aguallarcdam1 <aguallarcdam1@gmail.com>
2021-04-16 19:24:58 +0200

    Añadidos comentarios en 'Personal' y correcion en buscarContactos

--- src/ut7/agenda/modelo/AgendaContactos.java
+++ src/ut7/agenda/modelo/AgendaContactos.java
@@ -15,194 +15,191 @@ import java.util.TreeSet;
  * @author Andrés & Javier
  */
 public class AgendaContactos {
-	private Map<Character, Set<Contacto>> agenda;
+    private Map<Character, Set<Contacto>> agenda;
 
-	public AgendaContactos() {
-		this.agenda = new TreeMap<>();
+    public AgendaContactos() {
+	this.agenda = new TreeMap<>();
+    }
+
+    /*
+     * Parte de Andrés. Se añade una entrada en el mapa 'agenda', comprobando si la
+     * clave ya está dentro del mapa.
+     */
+    public void añadirContacto(Contacto contacto) {
+	if (!agenda.containsKey(contacto.getPrimeraLetra())) {
+	    TreeSet<Contacto> value = new TreeSet<>();
+	    value.add(contacto);
+	    agenda.put(contacto.getPrimeraLetra(), value);
+	} else {
+	    agenda.get(contacto.getPrimeraLetra()).add(contacto);
 	}
+    }
 
-	/*
-	 * Parte de Andrés. Se añade una entrada en el mapa 'agenda', comprobando si la
-	 * clave ya está dentro del mapa.
-	 */
-	public void añadirContacto(Contacto contacto) {
-		if (!agenda.containsKey(contacto.getPrimeraLetra())) {
-			TreeSet<Contacto> value = new TreeSet<>();
-			value.add(contacto);
-			agenda.put(contacto.getPrimeraLetra(), value);
-		} else {
-			agenda.get(contacto.getPrimeraLetra()).add(contacto);
-		}
-	}
+    /*
+     * Parte de Javier.Devuelve todos los contactos de la letra dada como parametro
+     */
+    public Set<Entry<Character, Set<Contacto>>> contactosEnLetra(char letra) {
+	Set<Map.Entry<Character, Set<Contacto>>> entradas = agenda.entrySet();
+	for (Entry<Character, Set<Contacto>> entrada : entradas) {
+	    Set<Contacto> contactos = entrada.getValue();
+	    for (Contacto contacto : contactos) {
+		if (contacto.getPrimeraLetra() == letra) {
+		    entradas.add(entrada);
 
-	/*
-	 * Parte de Javier.Devuelve todos los contactos de la letra dada como parametro
-	 */
-	public Set<Entry<Character, Set<Contacto>>> contactosEnLetra(char letra) {
-		Set<Map.Entry<Character, Set<Contacto>>> entradas = agenda.entrySet();
-		for (Entry<Character, Set<Contacto>> entrada : entradas) {
-			Set<Contacto> contactos = entrada.getValue();
-			for (Contacto contacto : contactos) {
-				if (contacto.getPrimeraLetra() == letra) {
-					entradas.add(entrada) ;
-					
-				}
-				
-			}
-	}
-		return entradas;
-		
-	}
-
-	/*
-	 * Parte de Andrés. Devuelve el total de contactos que hay almacenados en la
-	 * agenda.
-	 * 
-	 */
-	public int totalContactos() {
-		int totalContactos = 0;
-		Set<Character> keys = agenda.keySet();
-		for (Character character : keys) {
-			totalContactos += totalContactos + agenda.get(character).size();
-		}
-		return totalContactos;
-	}
-
-	/*
-	 * Parte de Javier Representacion textual
-	 */
-	@Override
-	public String toString() {
-
-		return null;
-	}
-
-	/*
-	 * Parte de Andrés. Se pasa un texto como parámetro y se devuelve una lista con
-	 * los contactos que contengan ese texto en su nombre o apellidos.
-	 */
-	public ArrayList<Contacto> buscarContactos(String texto) {
-		ArrayList<Contacto> contactosBuscados = new ArrayList<>();
-
-		Set<Map.Entry<Character, Set<Contacto>>> entradas = agenda.entrySet();
-		for (Entry<Character, Set<Contacto>> entrada : entradas) {
-			Set<Contacto> contactos = entrada.getValue();
-			for (Contacto contacto : contactos) {
-				if (contacto.getApellidos().contains(texto) || contacto.getNombre().contains(texto)) {
-					contactosBuscados.add(contacto);
-				}
-			}
-		}
-		return contactosBuscados;
-
-	}
-
-	/*
-	 * Parte de Javier. Devuelve todos los contocatos que sean personales y que tengan la letra pasada como parametro
-	 */
-	public List<Personal> personalesEnLetra(char letra) {
-	
-			
-		ArrayList<Personal> personalLetra = new ArrayList<>();
-
-		Set<Map.Entry<Character, Set<Contacto>>> entradas = agenda.entrySet();
-		for (Entry<Character, Set<Contacto>> entrada : entradas) {
-			Set<Contacto> contactos = entrada.getValue();
-			for (Contacto contacto : contactos) {
-				if (contacto instanceof Personal && contacto.getPrimeraLetra() == letra) {
-					personalLetra.add((Personal) contacto);
-				}
-				else if (agenda.containsKey(letra)== false) {
-					return null;
-					
-				}
-			}
-	}
-		return personalLetra;
-	}
-
-	/*
-	 * Parte de Andrés. Devuelve una lista con los contactos personales que cumplen
-	 * años.
-	 */
-	public ArrayList<Personal> felicitar() {
-		ArrayList<Personal> contactosFelicitados = new ArrayList<>();
-
-		Set<Map.Entry<Character, Set<Contacto>>> entradas = agenda.entrySet();
-		for (Entry<Character, Set<Contacto>> entrada : entradas) {
-			Set<Contacto> contactos = entrada.getValue();
-			for (Contacto contacto : contactos) {
-				if (contacto instanceof Personal) {
-					contactosFelicitados.add((Personal) contacto);
-				}
-			}
 		}
 
-		return contactosFelicitados;
+	    }
 	}
+	return entradas;
 
-	/*
-	 * Parte de Javier Devuelve un nuevo map en el que aparecen solo contactos
-personales pero organizados de forma que la clave en el nuevo
-map es la relación (un enumerado) y el valor asociado una
-colección List de cadenas con los apellidos y nombre de todos los
-contactos personales que hay en la agenda
-	 */
-	public Map<Relacion, List<String>> personalesPorRelacion() {
-		Map<Relacion, List<String>> perso = new TreeMap<>();
-		Iterator<Map.Entry<Character, Set<Contacto>>> it = agenda.entrySet().iterator();
-		while(it.hasNext()) {
-			Map.Entry<Character, Set<Contacto>> mp = it.next();
-			for(Contacto con : mp.getValue()) {
-				if(con instanceof Personal) {
-					String str = con.getNombre() + con.getApellidos();
-					Relacion rel = ((Personal)con).getRelacion();
-					
-						if(perso.containsKey(rel)) {
-							perso.get(rel).add(str);
-						}
-						else {
-							
-							List<String> personales = new ArrayList<>();
-							personales.add(str);
-							perso.put(rel, personales);
-							
-						}
-				}
-				
-			}
-			
-			
+    }
+
+    /*
+     * Parte de Andrés. Devuelve el total de contactos que hay almacenados en la
+     * agenda.
+     * 
+     */
+    public int totalContactos() {
+	int totalContactos = 0;
+	Set<Character> keys = agenda.keySet();
+	for (Character character : keys) {
+	    totalContactos += totalContactos + agenda.get(character).size();
+	}
+	return totalContactos;
+    }
+
+    /*
+     * Parte de Javier Representacion textual
+     */
+    @Override
+    public String toString() {
+
+	return null;
+    }
+
+    /*
+     * Parte de Andrés. Se pasa un texto como parámetro y se devuelve una lista con
+     * los contactos que contengan ese texto en su nombre o apellidos.
+     */
+    public ArrayList<Contacto> buscarContactos(String texto) {
+	ArrayList<Contacto> contactosBuscados = new ArrayList<>();
+
+	Set<Map.Entry<Character, Set<Contacto>>> entradas = agenda.entrySet();
+	for (Entry<Character, Set<Contacto>> entrada : entradas) {
+	    Set<Contacto> contactos = entrada.getValue();
+	    for (Contacto contacto : contactos) {
+		if (contacto.getApellidos().contains(texto.toUpperCase())
+			|| contacto.getNombre().contains(texto.toUpperCase())) {
+		    contactosBuscados.add(contacto);
 		}
-		
-		return perso;
-
+	    }
 	}
+	return contactosBuscados;
 
-	/*
-	 * Parte de Andrés. Devuelve una lista de contactos personales ordenados por
-	 * fecha de nacimiento.
-	 */
-	public List<Personal> personalesOrdenadosPorFechaNacimiento(char letra) {
-		ArrayList<Personal> personalesOrdenadosFecha = new ArrayList<>();
+    }
 
-		Set<Contacto> contactos = agenda.get(letra);
-		for (Contacto contacto : contactos) {
-			if (contacto instanceof Personal) {
-				personalesOrdenadosFecha.add((Personal) contacto);
-			}
+    /*
+     * Parte de Javier. Devuelve todos los contocatos que sean personales y que
+     * tengan la letra pasada como parametro
+     */
+    public List<Personal> personalesEnLetra(char letra) {
+
+	ArrayList<Personal> personalLetra = new ArrayList<>();
+
+	Set<Map.Entry<Character, Set<Contacto>>> entradas = agenda.entrySet();
+	for (Entry<Character, Set<Contacto>> entrada : entradas) {
+	    Set<Contacto> contactos = entrada.getValue();
+	    for (Contacto contacto : contactos) {
+		if (contacto instanceof Personal && contacto.getPrimeraLetra() == letra) {
+		    personalLetra.add((Personal) contacto);
+		} else if (agenda.containsKey(letra) == false) {
+		    return null;
+
 		}
-		Collections.sort(personalesOrdenadosFecha, new Comparator<Personal>() {
+	    }
+	}
+	return personalLetra;
+    }
 
-			@Override
-			public int compare(Personal c1, Personal c2) {
+    /*
+     * Parte de Andrés. Devuelve una lista con los contactos personales que cumplen
+     * años.
+     */
+    public ArrayList<Personal> felicitar() {
+	ArrayList<Personal> contactosFelicitados = new ArrayList<>();
 
-				return c1.getFechaNacimiento().compareTo(c2.getFechaNacimiento());
+	Set<Map.Entry<Character, Set<Contacto>>> entradas = agenda.entrySet();
+	for (Entry<Character, Set<Contacto>> entrada : entradas) {
+	    Set<Contacto> contactos = entrada.getValue();
+	    for (Contacto contacto : contactos) {
+		if (contacto instanceof Personal) {
+		    contactosFelicitados.add((Personal) contacto);
+		}
+	    }
+	}
 
-			}
-		});
-		return personalesOrdenadosFecha;
+	return contactosFelicitados;
+    }
+
+    /*
+     * Parte de Javier Devuelve un nuevo map en el que aparecen solo contactos
+     * personales pero organizados de forma que la clave en el nuevo map es la
+     * relación (un enumerado) y el valor asociado una colección List de cadenas con
+     * los apellidos y nombre de todos los contactos personales que hay en la agenda
+     */
+    public Map<Relacion, List<String>> personalesPorRelacion() {
+	Map<Relacion, List<String>> perso = new TreeMap<>();
+	Iterator<Map.Entry<Character, Set<Contacto>>> it = agenda.entrySet().iterator();
+	while (it.hasNext()) {
+	    Map.Entry<Character, Set<Contacto>> mp = it.next();
+	    for (Contacto con : mp.getValue()) {
+		if (con instanceof Personal) {
+		    String str = con.getNombre() + con.getApellidos();
+		    Relacion rel = ((Personal) con).getRelacion();
+
+		    if (perso.containsKey(rel)) {
+			perso.get(rel).add(str);
+		    } else {
+
+			List<String> personales = new ArrayList<>();
+			personales.add(str);
+			perso.put(rel, personales);
+
+		    }
+		}
+
+	    }
 
 	}
+
+	return perso;
+
+    }
+
+    /*
+     * Parte de Andrés. Devuelve una lista de contactos personales ordenados por
+     * fecha de nacimiento.
+     */
+    public List<Personal> personalesOrdenadosPorFechaNacimiento(char letra) {
+	ArrayList<Personal> personalesOrdenadosFecha = new ArrayList<>();
+
+	Set<Contacto> contactos = agenda.get(letra);
+	for (Contacto contacto : contactos) {
+	    if (contacto instanceof Personal) {
+		personalesOrdenadosFecha.add((Personal) contacto);
+	    }
+	}
+	Collections.sort(personalesOrdenadosFecha, new Comparator<Personal>() {
+
+	    @Override
+	    public int compare(Personal c1, Personal c2) {
+
+		return c1.getFechaNacimiento().compareTo(c2.getFechaNacimiento());
+
+	    }
+	});
+	return personalesOrdenadosFecha;
+
+    }
 
 }
--- src/ut7/agenda/modelo/Personal.java
+++ src/ut7/agenda/modelo/Personal.java
@@ -7,56 +7,90 @@ import java.time.format.DateTimeFormatter;
  * @author Andrés & Javier
  */
 public class Personal extends Contacto {
-	private LocalDate fechaNacimiento;
-	private Relacion relacion;
+    private LocalDate fechaNacimiento;
+    private Relacion relacion;
 
-	public Personal(String nombre, String apellidos, String telefono, String email, String fechaNacimiento,
-			Relacion relacion) {
-		super(nombre, apellidos, telefono, email);
-		this.setFechaNacimiento(fechaNacimiento);
-		this.setRelacion(relacion);
+    /**
+     * 
+     * Constructor de Personal
+     */
+    public Personal(String nombre, String apellidos, String telefono, String email, String fechaNacimiento,
+	    Relacion relacion) {
+	super(nombre, apellidos, telefono, email);
+	this.setFechaNacimiento(fechaNacimiento);
+	this.setRelacion(relacion);
+    }
+
+    /**
+     * 
+     * Accesor de relacion
+     */
+    public Relacion getRelacion() {
+	return relacion;
+    }
+
+    /**
+     * 
+     * Mutador de relacion
+     */
+    public void setRelacion(Relacion relacion) {
+	this.relacion = relacion;
+    }
+
+    /**
+     * 
+     * Accesor de fechaNacimiento
+     */
+    public LocalDate getFechaNacimiento() {
+	return fechaNacimiento;
+    }
+
+    /**
+     * 
+     * Mutador de fechaNacimiento
+     */
+    public void setFechaNacimiento(String fechaNacimiento) {
+	DateTimeFormatter formateadorFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	this.fechaNacimiento = LocalDate.parse(fechaNacimiento, formateadorFecha);
+    }
+
+    /**
+     * 
+     * Redefinición del método abstracto getFirmaEmail de la clase padre Contacto
+     */
+    @Override
+    public String getFirmaEmail() {
+
+	return "Un abrazo!!";
+    }
+
+    /**
+     * 
+     * Método que devuelve true si la fecha del día de ejecucón del método coincide
+     * con la fecha de la variable fechaNacimiento
+     */
+    public boolean esCumpleaños() {
+	LocalDate today = LocalDate.now();
+	if (fechaNacimiento.getDayOfMonth() == today.getDayOfMonth()
+		&& fechaNacimiento.getMonthValue() == today.getMonthValue()) {
+	    return true;
+	} else {
+	    return false;
 	}
 
-	public Relacion getRelacion() {
-		return relacion;
-	}
+    }
 
-	public void setRelacion(Relacion relacion) {
-		this.relacion = relacion;
-	}
+    /**
+     * 
+     * Método toString de la clase PersonaL. Se le llama al toString de la clase
+     * padre Contacto
+     */
+    @Override
+    public String toString() {
 
-	public LocalDate getFechaNacimiento() {
-		return fechaNacimiento;
-	}
-
-	public void setFechaNacimiento(String fechaNacimiento) {
-		DateTimeFormatter formateadorFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		this.fechaNacimiento = LocalDate.parse(fechaNacimiento, formateadorFecha);
-	}
-
-	@Override
-	public String getFirmaEmail() {
-
-		return "Un abrazo!!";
-	}
-
-	public boolean esCumpleaños() {
-		LocalDate today = LocalDate.now();
-		if (fechaNacimiento.getDayOfMonth() == today.getDayOfMonth()
-				&& fechaNacimiento.getMonthValue() == today.getMonthValue()) {
-			return true;
-		} else {
-			return false;
-		}
-
-	}
-
-	@Override
-	public String toString() {
-
-		return super.toString() + "\nFecha nacimiento: "
-				+ this.getFechaNacimiento().format(DateTimeFormatter.ofPattern("dd MMM. yyyy")) + "\nRelación: "
-				+ this.getRelacion() + "\n";
-	}
+	return super.toString() + "\nFecha nacimiento: "
+		+ this.getFechaNacimiento().format(DateTimeFormatter.ofPattern("dd MMM yyyy")) + "\nRelación: "
+		+ this.getRelacion() + "\n";
+    }
 
 }
