f21cf87df3821b870bfded20feb373c4647798d9
prog <prog@3MASANZ-A26-11>
2021-05-19 13:34:27 +0200

    Realizados los pasos previos (crear paquetes, etc.). Andrés & Javier

---
+++ src/agenda/interfaz/GuiAgenda.java
@@ -0,0 +1,172 @@
+package agenda.interfaz;
+
+import agenda.modelo.AgendaContactos;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GuiAgenda extends Application {
+	private AgendaContactos agenda;
+	private MenuItem itemImportar;
+	private MenuItem itemExportarPersonales;
+	private MenuItem itemSalir;
+
+	private MenuItem itemBuscar;
+	private MenuItem itemFelicitar;
+
+	private MenuItem itemAbout;
+
+	private TextArea areaTexto;
+
+	private RadioButton rbtListarTodo;
+	private RadioButton rbtListarSoloNumero;
+	private Button btnListar;
+
+	private Button btnPersonalesEnLetra;
+	private Button btnPersonalesOrdenadosPorFecha;
+
+	private TextField txtBuscar;
+
+	private Button btnClear;
+	private Button btnSalir;
+
+	@Override
+	public void start(Stage stage) {
+		agenda = new AgendaContactos(); // el modelo
+
+		BorderPane root = crearGui();
+
+		Scene scene = new Scene(root, 1100, 700);
+		stage.setScene(scene);
+		stage.setTitle("Agenda de contactos");
+		scene.getStylesheets().add(getClass().getResource("/application.css")
+		                    .toExternalForm());
+		stage.show();
+
+	}
+
+	private BorderPane crearGui() {
+		BorderPane panel = new BorderPane();
+		panel.setTop(crearBarraMenu());
+		panel.setCenter(crearPanelPrincipal());
+		return panel;
+	}
+
+	private BorderPane crearPanelPrincipal() {
+		BorderPane panel = new BorderPane();
+		panel.setPadding(new Insets(10));
+		panel.setTop(crearPanelLetras());
+
+		areaTexto = new TextArea();
+		areaTexto.getStyleClass().add("textarea");
+		panel.setCenter(areaTexto);
+
+		panel.setLeft(crearPanelBotones());
+		return panel;
+	}
+
+	private VBox crearPanelBotones() {
+		// a completar
+		VBox panel = new VBox();
+
+		return panel;
+	}
+
+	private GridPane crearPanelLetras() {
+		// a completar
+		GridPane panel = new GridPane();
+
+		return panel;
+	}
+
+	private MenuBar crearBarraMenu() {
+		// a completar
+		MenuBar barra = new MenuBar();
+
+		return barra;
+	}
+
+	private void importarAgenda() {
+		// a completar
+
+	}
+
+	private void exportarPersonales() {
+		// a completar
+
+	}
+
+	/**
+	 *  
+	 */
+	private void listar() {
+		clear();
+		// a completar
+
+	}
+
+	private void personalesOrdenadosPorFecha() {
+		clear();
+		// a completar
+
+	}
+
+	private void contactosPersonalesEnLetra() {
+		clear();
+		// a completar
+
+	}
+
+	private void contactosEnLetra(char letra) {
+		clear();
+		// a completar
+	}
+
+	private void felicitar() {
+		clear();
+		// a completar
+
+	}
+
+	private void buscar() {
+		clear();
+		// a completar
+
+		cogerFoco();
+
+	}
+
+	private void about() {
+		// a completar
+
+	}
+
+	private void clear() {
+		areaTexto.setText("");
+	}
+
+	private void salir() {
+		Platform.exit();
+	}
+
+	private void cogerFoco() {
+		txtBuscar.requestFocus();
+		txtBuscar.selectAll();
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
--- src/agenda/io/AgendaIO.java
+++ src/agenda/io/AgendaIO.java
@@ -2,7 +2,6 @@ package agenda.io;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -23,82 +22,80 @@ import agenda.modelo.Relacion;
  * @author Andrés & Javier
  */
 public class AgendaIO {
-    /**
-     * Se extrae los datos del fichero que se proporciona y se añaden los contactos
-     * a la agenda.
-     * 
-     * @param agenda
-     * @param nombre
-     * @return número de errores cuando se lee el fichero
-     */
-    public static int importar(AgendaContactos agenda, String nombre) {
-	File fichero = new File(nombre);
-	int lineas_erroneas = 0;
-	Scanner sc = null;
-	try {
-	    sc = new Scanner(fichero);
-	    while (sc.hasNextLine()) {
+	/**
+	 * Se extrae los datos del fichero que se proporciona y se añaden los contactos
+	 * a la agenda.
+	 * 
+	 * @param agenda
+	 * @param nombre
+	 * @return número de errores cuando se lee el fichero
+	 */
+	public static int importar(AgendaContactos agenda, String nombre) {
+
+		int lineas_erroneas = 0;
+		Scanner sc = null;
 		try {
-		    String linea = sc.nextLine();
-		    agenda.añadirContacto(parsearLinea(linea));
-		} catch (DateTimeParseException e) {
+			sc = new Scanner((AgendaIO.class.getClassLoader().getResourceAsStream(nombre)));
+			while (sc.hasNextLine()) {
+				try {
+					String linea = sc.nextLine();
+					agenda.añadirContacto(parsearLinea(linea));
+				} catch (DateTimeParseException e) {
 
-		    lineas_erroneas++;
-		} catch (NumberFormatException e) {
+					lineas_erroneas++;
+				} catch (NumberFormatException e) {
 
-		    lineas_erroneas++;
-		} catch (IllegalArgumentException e) {
+					lineas_erroneas++;
+				} catch (IllegalArgumentException e) {
 
-		    lineas_erroneas++;
+					lineas_erroneas++;
+				}
+			}
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
 		}
-	    }
-	} catch (FileNotFoundException e) {
-	    System.out.println("Error al cargar el fichero: " + e.getMessage());
-	} finally {
-	    if (sc != null) {
-		sc.close();
-	    }
+		return lineas_erroneas;
 	}
-	return lineas_erroneas;
-    }
 
-    /**
-     * Se le pasa una línea la cual se le extrae los datos para crear un Contacto
-     * 
-     * @param linea
-     * @return un Contacto
-     * @throws DateTimeParseException
-     * @throws IllegalArgumentException
-     * @throws NumberFormatException
-     */
-    private static Contacto parsearLinea(String linea)
-	    throws DateTimeParseException, IllegalArgumentException, NumberFormatException {
-	String[] datos = linea.split("\\,+");
-	if (Integer.parseInt(datos[0].trim()) == 1) {
-	    return new Profesional(datos[1].trim(), datos[2].trim(), datos[3].trim(), datos[4].trim(), datos[5].trim());
-	} else {
-	    return new Personal(datos[1].trim(), datos[2].trim(), datos[3].trim(), datos[4].trim(), datos[5].trim(),
-		    Relacion.valueOf(datos[6].toUpperCase().trim()));
+	/**
+	 * Se le pasa una línea la cual se le extrae los datos para crear un Contacto
+	 * 
+	 * @param linea
+	 * @return un Contacto
+	 * @throws DateTimeParseException
+	 * @throws IllegalArgumentException
+	 * @throws NumberFormatException
+	 */
+	private static Contacto parsearLinea(String linea)
+			throws DateTimeParseException, IllegalArgumentException, NumberFormatException {
+		String[] datos = linea.split("\\,+");
+		if (Integer.parseInt(datos[0].trim()) == 1) {
+			return new Profesional(datos[1].trim(), datos[2].trim(), datos[3].trim(), datos[4].trim(), datos[5].trim());
+		} else {
+			return new Personal(datos[1].trim(), datos[2].trim(), datos[3].trim(), datos[4].trim(), datos[5].trim(),
+					Relacion.valueOf(datos[6].toUpperCase().trim()));
+		}
 	}
-    }
 
-    /**
-     * 
-     * El metodo guarda en el fichero de texto los contactos personales agrupados
-     * por relación
-     * 
-     * @param agenda
-     * @param nombre
-     * @throws IOException
-     */
-    public static void exportarPersonales(AgendaContactos agenda, String nombre) throws IOException {
-	File f = new File(nombre);
-	PrintWriter salida = new PrintWriter(new BufferedWriter(new FileWriter(f)));
+	/**
+	 * 
+	 * El metodo guarda en el fichero de texto los contactos personales agrupados
+	 * por relación
+	 * 
+	 * @param agenda
+	 * @param nombre
+	 * @throws IOException
+	 */
+	public static void exportarPersonales(AgendaContactos agenda, String nombre) throws IOException {
+		File f = new File(nombre);
+		PrintWriter salida = new PrintWriter(new BufferedWriter(new FileWriter(f)));
 
-	Map<Relacion, List<String>> map = agenda.personalesPorRelacion();
-	map.forEach((key, value) -> salida.println(key + "\n\t" + value));
-	salida.close();
+		Map<Relacion, List<String>> map = agenda.personalesPorRelacion();
+		map.forEach((key, value) -> salida.println(key + "\n\t" + value));
+		salida.close();
 
-    }
+	}
 
 }
